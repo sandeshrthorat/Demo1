84deae4147910a865044f959580beecc9d6ddd83
user <user@DESKTOP-D0NFV9L>
2022-01-12 21:49:37 +0530

    Sandesh

---
+++ first project/src/srt/Welcome.java
@@ -0,0 +1,15 @@
+package srt;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("main with String");
+	}
+	
+	public static void main(int args) {
+		System.out.println("main with int");
+	}
+	
+	public static void main(char[] args) {
+		System.out.println("main with char[]");
+	}
+}
---
+++ first project/src/srt/Welcome2.java
@@ -0,0 +1,7 @@
+package srt;
+
+public class Welcome2 extends Welcome{
+	 		public static void main(String[] args) {
+				System.out.println("Welcome Thorat");
+			}
+}
